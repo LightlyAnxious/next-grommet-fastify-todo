b39f8d405b6d56b5c015ab7d139a3a87da47ad67
LightlyAnxious <daniilprikhodko87@gmail.com>
2022-07-14 21:30:30 +0300

    Add data fetching in index page

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import { Grommet, grommet } from "grommet";
+import { deepMerge } from "grommet/utils";
 import { useCallback, useState } from "react";
+import { css } from "styled-components";
 import Layout from "../components/layout";
 
 import "../styles/globals.css";
+
+const customTheme = {
+  textInput: {
+    extend: css`
+      padding-left: 48px;
+    `,
+  },
+};
 
 function MyApp({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false);
@@ -12,7 +22,11 @@ function MyApp({ Component, pageProps }) {
   }, []);
 
   return (
-    <Grommet full theme={grommet} themeMode={darkMode ? "dark" : "light"}>
+    <Grommet
+      full
+      theme={deepMerge(grommet, customTheme)}
+      themeMode={darkMode ? "dark" : "light"}
+    >
       <Layout darkMode={darkMode} onToggleMode={onToggleMode}>
         <Component {...pageProps} />
       </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,39 @@
 import styled from "styled-components";
-import { Box, Card, Grid, Heading, TextInput } from "grommet";
+import { Box, Button, Card, Grid, Heading, TextInput } from "grommet";
 import Layout from "../components/layout";
 
 import mockTodos from "../mocks";
 import { Add } from "grommet-icons";
+import { useState } from "react";
+
+const LIMIT = 6;
+
+const InputField = styled(Box)`
+  position: relative;
+`;
+
+const AddButton = styled(Button).attrs({ icon: <Add /> })`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: fit-content;
+  transition: all 0.25s ease-in-out;
+
+  svg,
+  path {
+    fill: currentColor;
+    stroke: currentColor;
+  }
+
+  :hover,
+  :focus {
+    color: #7d4cdb;
+  }
+`;
 
 const Cards = ({ list }) => {
   return (
-    <>
+    <Box margin={{ bottom: "large" }}>
       {list.map((m) => {
         const { id, userId, title, completed } = m;
 
@@ -17,20 +43,50 @@ const Cards = ({ list }) => {
           </Card>
         );
       })}
-    </>
+    </Box>
   );
 };
 
-export default function Dashboard({ children }) {
+export default function Dashboard({ todos, children }) {
+  const [count, setCount] = useState(1);
+
+  const todoChunk = [...(todos ?? [])].slice(0, count * LIMIT);
+
+  const isShowMoreButton = count * LIMIT < todos.length;
   return (
     <>
       <Grid
-        margin={{ vertical: "medium", horizontal: "auto" }}
+        alignSelf="center"
+        margin={{ vertical: "medium" }}
         width={{ max: "600px" }}
       >
-        <TextInput icon={<Add />} />
-        <Cards list={mockTodos} />
+        <InputField>
+          <TextInput pad={{ left: "48px" }} />
+          <AddButton secondary />
+        </InputField>
+        <Cards list={todoChunk} />
+        {isShowMoreButton && (
+          <Button
+            primary
+            label="Show more"
+            onClick={() => {
+              setCount((prev) => ++prev);
+            }}
+          />
+        )}
       </Grid>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const todos = await (
+    await fetch("https://jsonplaceholder.typicode.com/todos")
+  )?.json();
+
+  return {
+    props: {
+      todos,
+    },
+  };
 }
